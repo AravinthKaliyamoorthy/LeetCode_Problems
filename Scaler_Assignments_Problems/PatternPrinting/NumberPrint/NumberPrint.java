bf5bc0663940e1878e5671d3f662912cb681520c
Aravinth K <82627549+AravinthKaliyamoorthy@users.noreply.github.com>
2023-09-09 04:09:30 +0000

    problem for carry, subarray and patter

---
+++ Scaler_Assignments_Problems/PatternPrinting/NumberPrint/NumberPrint.java
@@ -0,0 +1,30 @@
+package Scaler_Assignments_Problems.PatternPrinting.NumberPrint;
+
+import java.util.*;
+
+public class NumberPrint{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input : ");
+        int size = in.nextInt();
+        for(int i = 0; i < size; i++){
+            for(int j = i+1; j < size; j++){
+                System.out.print("0");
+            }
+            for(int k = size-i-1; k < size; k++){
+                System.out.print(size-k);
+            }
+            System.out.println();
+        }
+        System.out.println("----------------");
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print(j+1);
+            }
+            for(int k = i+1; k < size; k++){
+                System.out.print("0");
+            }
+            System.out.println();
+        }
+    }    
+}
---
+++ Scaler_Assignments_Problems/Sub_Array/EvenSubarrays/EvenSubarrays.java
@@ -0,0 +1,28 @@
+package Scaler_Assignments_Problems.Sub_Array.EvenSubarrays;
+
+public class EvenSubarrays {
+    public static void main(String[] args) {
+        // int arr[] = {2, 4, 8, 6};
+        int arr[] = {2, 4, 8, 7, 6};
+
+        //brute force approach
+        bruteForce(arr, arr.length);
+    }    
+
+    public static void bruteForce(int arr[], int arraySize){
+        int ans = 0;
+        for(int i = 0; i < arraySize; i++){
+            if(arr[i] % 2 == 0){
+                for(int j = i+1; j < arraySize; j++){
+                    if(arr[j] % 2 == 0 && ((j - i) % 2 == 1)){
+                        ans++;
+                        System.out.println("i and j : " + i + " --- " + j);
+                    }
+                }
+            }
+        }
+        if(ans > 1){
+            System.out.println("YES");
+        }
+    }
+}
