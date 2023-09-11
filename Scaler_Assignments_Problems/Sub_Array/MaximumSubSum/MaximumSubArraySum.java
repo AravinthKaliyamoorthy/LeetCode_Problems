da20bd0118748e4920cf1c91f6661fd08f860ae6
Aravinth K <82627549+AravinthKaliyamoorthy@users.noreply.github.com>
2023-09-11 07:30:14 +0000

    adding maximum subarray sum

---
+++ Scaler_Assignments_Problems/Sub_Array/MaximumSubSum/MaximumSubArraySum.java
@@ -0,0 +1,46 @@
+package Scaler_Assignments_Problems.Sub_Array.MaximumSubSum;
+
+public class MaximumSubArraySum {
+    public static void main(String[] args) {
+        int arr[] = {-1, 2, -1, 3, -1, -4, -2, -1, 7, -2};
+
+        //brute force approach -> TC: O(N^2) SC: O(1)
+        bruteForce(arr, arr.length);
+
+        //optimized approach -> TC: O(N) SC: O(1)
+        optimizedApproach(arr, arr.length);
+    }
+
+    public static void bruteForce(int arr[], int arraySize){
+        int maxSum = Integer.MIN_VALUE, sum = 0;
+        for(int i = 0; i < arraySize; i++){
+            sum = 0;
+            for(int j = i; j < arraySize; j++){
+                sum += arr[j];
+                if(sum >= maxSum){
+                    maxSum = sum;
+                }
+            }
+        }
+        System.out.println("Maximum subarray sum : "+maxSum);
+
+    }
+
+    public static void optimizedApproach(int arr[], int arraySize){
+        int currSum = arr[0];
+        int maxSum = arr[0];
+        for(int i = 1; i < arraySize; i++){
+            // if the current sum is negative means if you sum with i element it becomes less i index 
+            // element so butter to i index element as current sum it makes as new subarray starting here
+            if(currSum < 0){
+                currSum = arr[i];
+            }
+            else{
+                currSum += arr[i];
+            }
+            maxSum = Math.max(maxSum, currSum);
+        }
+        System.out.println("Maximum subarray sum : "+maxSum);
+    }
+
+}
